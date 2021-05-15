3e64fda570a1c7fce285424f0074b0eb0c83ea6d
Farrukh Karimov <farrukh.karimov@huawei.com>
2021-05-16 00:41:24 +0300

    Configuring Procfile console worker

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: java $JAVA_OPTS -jar target/BotNetLoadBalancer2-1.0-SNAPSHOT.jar --server.port=$PORT
-worker: java src/main/java/com/example/Main.java
+web: java $JAVA_OPTS -jar target/BotNetLoadBalancer2-1.0-SNAPSHOT.jar -web --server.port=$PORT
+worker: java $JAVA_OPTS -jar target/BotNetLoadBalancer2-1.0-SNAPSHOT.jar -console --server.port=$PORT
--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -4,12 +4,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import static com.example.BotNetUtils.httpsGETRequest;
+
 @SpringBootApplication
 @EnableScheduling
 public class ServingWebContentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServingWebContentApplication.class, args);
+        if (args[0].equals("-web")) {
+            SpringApplication.run(ServingWebContentApplication.class, args);
+        } else {
+            final String helloUrl = "https://vious.herokuapp.com/hello";
+            while (true) {
+                System.out.println("Doing health check from additional worker");
+                httpsGETRequest(helloUrl);
+                try {
+                    Thread.sleep(60000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
